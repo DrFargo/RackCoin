644297993b2f8e76f256e02e07ce8fac3558669d
DrFargo <32229490+DrFargo@users.noreply.github.com>
2021-03-27 06:57:45 +0100

    Update lint_06_script.sh

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,6 +6,8 @@
 
 export LC_ALL=C
 
+git update-index --add --chmod=+x test/lint/git-subtree-check.sh
+
 if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
   test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
 fi
